44fc40da28e579ecea1cb7aae51af832e27a8159
ravi9879 <ravigupta77389@gmail.com>
2025-05-27 19:44:11 +0530

    update1

--- src/files/Project.js
+++ src/files/Project.js
@@ -12,15 +12,15 @@ function Project() {
           <button className='hover_button' style={{ "width": "100%" }}>
             <a href='https://ecommerce-cyan-nine.vercel.app/'>Visit The Page</a>
           </button>
-          <li>Suduko Solver</li>
-          <img src={suduko} alt={suduko} className='pro' />
-          <button className='hover_button' style={{ "width": "100%" }}>
-            <a href='https://suduko-three.vercel.app/'>Visit The Page</a>
-          </button>
           <li>Crud App</li>
           <img src={Crud} alt={Crud} className='pro' />
           <button style={{ "width": "100%" }} className='hover_button'>
             <a href='https://crud-one-hazel.vercel.app/'>Visit The Page</a>  
+          </button>
+          <li>Suduko Solver</li>
+          <img src={suduko} alt={suduko} className='pro' />
+          <button className='hover_button' style={{ "width": "100%" }}>
+            <a href='https://suduko-three.vercel.app/'>Visit The Page</a>
           </button>
         </ol>
       </div>
